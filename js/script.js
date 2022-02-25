af48e6ca6c92d1ae0b46cb7f5c1aeffb119f0a85
hasnainhashmi <59309374+hasnainhashmi@users.noreply.github.com>
2022-02-25 14:27:48 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,19 @@
+
+  window.sr = ScrollReveal({
+    distance: '50px',
+    duration: 1000,
+    easing: 'ease',
+    scale: 0.8,
+    mobile: true,
+    reset: false,
+    viewFactor: 0.2,
+  });
+
+
+
+
+//edit by HH
+sr.reveal('.info,.to,.project-title,.text-container',{
+});
+
+//edit end
